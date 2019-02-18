9de8678eb14bbf6f6dd3460eb943af55892b5296
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2019-02-18 12:47:55 +0100

    Added base classes

--- ProductCatalogAPI/Startup.cs
+++ ProductCatalogAPI/Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using ProductCatalogAPI.Data;
 
 namespace ProductCatalogAPI
 {
@@ -24,6 +26,7 @@ namespace ProductCatalogAPI
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddMvc();
+			services.AddDbContext<CatalogContext>(options => options.UseSqlServer(Configuration["ConnectionString"]));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
